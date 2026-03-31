bcf2d0854b036194c6461949ae86b8a1250d9b98
John <johnlu@gliacloud.com>
2026-03-31 17:04:57 +0800

    feat: integrate GliaPlayerView with pause and resume functionality in App component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,52 @@
-import { View, StyleSheet } from 'react-native';
-import { GliaPlayerView } from 'react-native-glia-player';
+import React, { useMemo, useRef } from 'react';
+import { View, ScrollView, StyleSheet, Button } from 'react-native';
+import { GliaPlayerView, Commands } from '@gliacloud/react-native-glia-player';
+
+const getRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
 export default function App() {
+  const playerRef = useRef(null);
+
+  const handlePause = () => {
+    if (playerRef.current) {
+      Commands.pause(playerRef.current);
+    }
+  };
+
+  const handleResume = () => {
+    if (playerRef.current) {
+      Commands.resume(playerRef.current);
+    }
+  };
+  const dummyBoxes = useMemo(() => {
+    return Array.from({ length: 100 }).map((_, index) => (
+      <View key={index} style={styles.boxContainer}>
+        <View
+          style={[styles.randomBox, { backgroundColor: getRandomColor() }]}
+        />
+      </View>
+    ));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <GliaPlayerView color="#32a852" style={styles.box} />
+      <View style={styles.buttonContainer}>
+        <Button title="Pause" onPress={handlePause} />
+        <Button title="Resume" onPress={handleResume} />
+      </View>
+      <ScrollView style={styles.scrollView}>{dummyBoxes}</ScrollView>
+      <View style={styles.playerContainer}>
+        <GliaPlayerView
+          ref={playerRef}
+          slotKey="gliacloud_app_test"
+          style={styles.gliaplayer}
+        />
+      </View>
     </View>
   );
 }
@@ -12,12 +54,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 100,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  boxContainer: {
+    marginBottom: 16,
+  },
+  randomBox: {
+    width: '100%',
+    height: 320,
+  },
+  playerContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 320,
+    height: 240,
+    elevation: 5,
+    zIndex: 5,
+  },
+  gliaplayer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
